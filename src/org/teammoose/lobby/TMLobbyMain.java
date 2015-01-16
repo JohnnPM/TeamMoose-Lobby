9914c4ec05d24552795b4cdc581f903aef01322d
xXJarJarXx <598Johnn897@gmail.com>
2015-01-16 16:39:51 -0500

    Added Main class

--- src/org/teammoose/lobby/TMLobbyMain.java
+++ src/org/teammoose/lobby/TMLobbyMain.java
@@ -8,9 +8,6 @@ package org.teammoose.lobby;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.samczsun.serverpinger.ServerPinger;
-import com.samczsun.serverpinger.StatusResponse;
-
 /**
  * 
  * @author 598Johnn897
@@ -25,8 +22,7 @@ public class TMLobbyMain extends JavaPlugin
 	
 	@Override public void onEnable()
 	{
-		StatusResponse resp = ServerPinger.POST_NETTY_REWRITE.fetchData("localhost", 25565);
-		int online = resp.players.online;
+		
 	}
 	
 	@Override public void onDisable()
